dd6ef2d10cb3bf5d6bc0ff0fc330d19a4b23151c
Zandi Milligan <zmilligan@ou.edu>
2019-03-07 15:36:28 -0600

    create PersonTest class

---
+++ PersonTest.java
@@ -0,0 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Lab 7
+ *
+ * Test class for the Person classes and their comparators.
+ *
+ * @author Zandi Millligan
+ * @version 2019-03-07
+ */
+public class PersonTest
+{
+	@Test
+	public void SickPersonTest()
+	{
+		
+	}
+	
+	@Test
+	public void HealthyPersonTest()
+	{
+		
+	}
+	
+	@Test
+	public void CompareSickPersonTest()
+	{
+		
+	}
+	
+	@Test
+	public void CompareHealthyPersonTest()
+	{
+		
+	}
+	
+	@Test
+	public void CompareDifferentTypesTest()
+	{
+		
+	}
+}
