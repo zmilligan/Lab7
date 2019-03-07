3ab5e0019f775288c1675d228414629ed6c22060
Zandi Milligan <zmilligan@ou.edu>
2019-03-07 15:46:42 -0600

    finished SickPerson stst

--- PersonTest.java
+++ PersonTest.java
@@ -14,7 +14,10 @@ public class PersonTest
 	@Test
 	public void SickPersonTest()
 	{
-		
+		SickPerson s1 = new SickPerson("Steve Rogers", 28, 2);
+		Assert.assertEquals("SickPerson name incorrect.", "Steve Rogers", s1.getName());
+		Assert.assertEquals("SickPerson age incorrect.", 28, s1.getAge(), 0.0001);
+		Assert.assertEquals("SickPerson toString incorrect.", "Steve Rogers, a 28-year old. Severity level 2", s1.toString());
 	}
 	
 	@Test
