733858e939ca91c5aee589ab232f92bdd8f44152
Zandi Milligan <zmilligan@ou.edu>
2019-03-07 15:19:27 -0600

    adjust for persons completed

--- Todo.java
+++ Todo.java
@@ -8,12 +8,21 @@ OU EMAIL____________: zmilligan@ou.edu
 Tasks:
 1. create skeleton for missing classes
 2. transfer javadoc info from html index into classes
+3. SickPerson, HealthyPerson toString methods
+4. Sick Person compareTo method
+5. HealthyPerson compareTo method
 
 Estimated Time (minutes):
 1. 25
 2. 20
+3. 4
+4. 5
+5. 5
 
 Actual Time (minutes):
 1. 21
 2. 28
+3. 3
+4. 8
+5. 3
 */}
