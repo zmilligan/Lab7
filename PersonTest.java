223c0e4e70ea05868ddcbd7bd65778b9f7c211b2
Zandi Milligan <zmilligan@ou.edu>
2019-03-08 11:49:48 -0600

    updated person info because I'm a nerd

--- PersonTest.java
+++ PersonTest.java
@@ -14,10 +14,10 @@ public class PersonTest
 	@Test
 	public void SickPersonTest()
 	{
-		SickPerson s1 = new SickPerson("Steve Rogers", 28, 2);
+		SickPerson s1 = new SickPerson("Steve Rogers", 30, 2);
 		Assert.assertEquals("SickPerson name incorrect.", "Steve Rogers", s1.getName());
-		Assert.assertEquals("SickPerson age incorrect.", 28, s1.getAge(), 0.0001);
-		Assert.assertEquals("SickPerson toString incorrect.", "Steve Rogers, a 28-year old. Severity level 2", s1.toString());
+		Assert.assertEquals("SickPerson age incorrect.", 30, s1.getAge(), 0.0001);
+		Assert.assertEquals("SickPerson toString incorrect.", "Steve Rogers, a 30-year old. Severity level 2", s1.toString());
 	}
 	
 	@Test
@@ -33,8 +33,8 @@ public class PersonTest
 	@Test
 	public void CompareSickPersonTest()
 	{
-		SickPerson s2 = new SickPerson("Phil Coulson", 30, 10);
-		SickPerson s3 = new SickPerson("Tony Stark", 34, 6);
+		SickPerson s2 = new SickPerson("Phil Coulson", 45, 10);
+		SickPerson s3 = new SickPerson("Tony Stark", 38, 6);
 		SickPerson s4 = new SickPerson("Pietro Maximoff", 22, 10);
 		Assert.assertEquals("First SickPerson compareTo test incorrect.", -1, s2.compareTo(s3), 0.0001);
 		Assert.assertEquals("Second SickPerson compareTo test incorrect.", 1, s3.compareTo(s2), 0.0001);
@@ -44,9 +44,9 @@ public class PersonTest
 	@Test
 	public void CompareHealthyPersonTest()
 	{
-		HealthyPerson h2 = new HealthyPerson("Bruce Banner", 32, "anger management issues follow-up");
+		HealthyPerson h2 = new HealthyPerson("Bruce Banner", 39, "anger management issues follow-up");
 		HealthyPerson h3 = new HealthyPerson("Clint Barton", 34, "quarterly physical");
-		HealthyPerson h4 = new HealthyPerson("Bruce Banner", 30, "quarterly physical");
+		HealthyPerson h4 = new HealthyPerson("Bruce Banner", 39, "quarterly physical");
 		Assert.assertEquals("First HealthyPerson compareTo test incorrect.", -1, h2.compareTo(h3), 0.0001);
 		Assert.assertEquals("Second HealthyPerson compareTo test incorrect.", 1, h3.compareTo(h2), 0.0001);
 		Assert.assertEquals("Third HealthyPerson compareTo test incorrect.", 0, h4.compareTo(h2), 0.0001);
